20e8627cc4e34e214e51b5f909a9c2ddbdc5435b
João Petel <petelsalvador15@gmail.com>
2025-10-13 14:49:37 +0100

    Resolucao de bugs e conflitos

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,9 @@
+
+//import {EXPO_PUBLIC_SUPABASE_ANON_KEY , EXPO_PUBLIC_SUPABASE_URL} from "@env"
+
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
 import { createClient } from '@supabase/supabase-js';
-import {EXPO_PUBLIC_SUPABASE_ANON_KEY , EXPO_PUBLIC_SUPABASE_URL} from "@env"
-
-
-export const supabase = createClient(EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
